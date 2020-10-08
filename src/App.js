9accfdd70e095ef3389ded7365c260b7ec461e1d
Frans Kbarek <63740351+frans250k@users.noreply.github.com>
2020-10-09 00:20:27 +0900

    tambah animasi background dengan react-particles.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Particles from 'react-particles-js'
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
@@ -6,11 +7,24 @@ import Rank from './components/Rank/Rank';
 
 import './App.css';
 
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 180,
+      density: {
+        enable: true,
+        value_area: 900
+      }
+
+    }
+  }
+      }
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Particles className='particles' params={particlesOptions} />
         <Navigation />
         <Logo />
         <Rank />
