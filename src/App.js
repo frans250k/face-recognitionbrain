b57669d3f7f251c11e7f2e1d42072c8ca1290259
Frans Kbarek <63740351+frans250k@users.noreply.github.com>
2020-10-13 14:42:37 +0900

    tambah fitur form register dan signin

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import Particles from 'react-particles-js';
 import Clarifai from 'clarifai';
 import Navigation from './components/Navigation/Navigation';
+import Signin from './components/Signin/Signin';
+import Register from './components/Register/Register';
 import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
@@ -31,7 +33,8 @@ class App extends Component {
     this.state = {
       input: '',
       imageUrl: '',
-      box: {}
+      box: {},
+      route: 'signin'
     }
   }
 
@@ -68,18 +71,32 @@ onButtonSubmit = () => {
       
     }
 
+  onRouteChange = (route) => {
+    this.setState({route: route})
+  }
+
   render() {
     return (
       <div className="App">
-        <Particles className='particles' params={particlesOptions} />
-        <Navigation />
-        <Logo />
-        <Rank />
-        <ImageLinkForm 
-        onInputChange={this.onInputChange} 
-        onButtonSubmit={this.onButtonSubmit} 
-        />
-        <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl} />
+        <Particles className='particles' 
+          params={particlesOptions} />
+        <Navigation onRouteChange={this.onRouteChange} />
+        { this.state.route === 'home'
+        ? <div>
+            <Logo />
+            <Rank />
+            <ImageLinkForm 
+            onInputChange={this.onInputChange} 
+            onButtonSubmit={this.onButtonSubmit} 
+            />
+            <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl} />
+            </div>
+        :  (
+          this.state.route === 'signin' ?
+          <Signin onRouteChange={this.onRouteChange} /> :
+          <Register onRouteChange={this.onRouteChange} />  
+          )  
+        }
       </div>
     );
 
--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -7,11 +7,11 @@ const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
 return (
   <div>
     <p className='f3'>
-      {'This magic brain will detect faces in your pictures. Get it try'}
+      {'This magic brain will detect faces in your pictures. Get it a try'}
       </p>    
       <div className='center'>
         <div className='form center pa4 br3 shadow-5'>
-        <input className='f4 pa3 w-70 center' type='text'onChange={onInputChange} />
+        <input placeholder='paste link picture' className='f4 pa3 w-70 center' type='text'onChange={onInputChange} />
         <button className='w-30 grow f4 link ph3 pv3 dib white bg-light-purple' onClick={onButtonSubmit}>Detect</button>
         </div>
       </div>
